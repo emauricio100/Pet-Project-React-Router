8bc9726152ac12b8ff70fa41b5857b1428f4c419
emauricio100 <emauricio@ucdavis.edu>
2023-05-31 14:18:36 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -14,7 +14,13 @@ import {
 
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Root />} />)
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<HomePage />} />
+      <Route path=":type" element={<HomePage />} />
+      <Route path=":type/:id" element={<PetDetailsPage />} />
+    </Route>
+  )
 );
 
 function App() {
--- src/components/root/index.js
+++ src/components/root/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Navigation from '../navigation';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navigation from "../navigation";
 // import Outlet
 
 const Root = () => {
-    return (
-        <>
-            <Navigation/>
-            {/* Add an Outlet*/}
-        </>
-    );
+  return (
+    <>
+      <Navigation />
+      <Outlet />
+      {/* Add an Outlet*/}
+    </>
+  );
 };
 
 export default Root;
