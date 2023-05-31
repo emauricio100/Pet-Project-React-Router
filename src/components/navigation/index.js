ca5dfc0e21c6d03a95d62c06a11a5e8a8e05b3dd
emauricio100 <emauricio@ucdavis.edu>
2023-05-31 15:05:31 -0700

    updates

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { getPetTypes } from '../../api/petfinder';
-import Logo from '../../assets/logo.svg';
-import Search from '../search';
+import React, { useEffect, useState } from "react";
+import { getPetTypes } from "../../api/petfinder";
+import Logo from "../../assets/logo.svg";
+import Search from "../search";
+import { NavLink } from "react-router-dom";
 
 // Import NavLink
 
@@ -24,26 +25,28 @@ const Navigation = () => {
         <Search />
       </div>
       <ul className="nav-links">
-        <li key={'all'}>
+        <li key={"all"}>
           {/* These links should be NavLink component and add a special active class name if its an active link */}
-          <a href="/"
-            className='nav-link'
-          >
+          <NavLink to="/" className="nav-link">
             All Pets
-          </a>
+          </NavLink>
         </li>
         {petTypes
           ? petTypes.map((type) => (
               <li key={type.name}>
                 {/* These links should be NavLink component and add a special active class name if its an active link */}
-                <a href={`/${type._links.self.href.split('/').pop()}`}
+                <NavLink
+                  to={`/${type._links.self.href.split("/").pop()}`}
                   key={type.name}
-                  className='nav-link'               >
-                  {type.name}s
-                </a>{' '}
+                  className={({ isActive }) =>
+                    `${isActive ? "nav-link nav-link-active" : "nav-link"}`
+                  }
+                >
+                  {type.name}
+                </NavLink>{" "}
               </li>
             ))
-          : 'Loading...'}
+          : "Loading..."}
       </ul>
     </nav>
   );
